d3e4ddbe077f621572d3ea44415640dd5118b02b
Jeffduran94 <jeffreydavisduran@gmail.com>
2021-04-21 23:37:37 -0600

    agregado logo svg y menu mobil completo

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ const navLink = document.querySelectorAll(".nav-link");
 
 navLink.forEach(n => n.addEventListener("click", closeMenu));
 
+// Remueve menu
+
 function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
